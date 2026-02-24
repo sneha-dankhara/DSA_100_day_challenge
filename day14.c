2d5a37341e77b2843da11cdf2eaca9aa3ee6d8ac
sneha-dankhara <Sneha.24651@stu.upes.ac.in>
2026-02-24 22:32:57 +0530

    Add files via upload

---
+++ day14.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+
+    printf("Enter size of matrix: ");
+    scanf("%d", &n);
+
+    int matrix[n][n];
+    int isIdentity = 1;
+
+    printf("Enter matrix elements:\n");
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+
+            if (i == j) {
+                if (matrix[i][j] != 1) {
+                    isIdentity = 0;
+                    break;
+                }
+            } 
+            else {
+                if (matrix[i][j] != 0) {
+                    isIdentity = 0;
+                    break;
+                }
+            }
+        }
+
+        if (isIdentity == 0)
+            break;
+    }
+
+    if (isIdentity)
+        printf("Identity Matrix\n");
+    else
+        printf("Not an Identity Matrix\n");
+
+    return 0;
+}
